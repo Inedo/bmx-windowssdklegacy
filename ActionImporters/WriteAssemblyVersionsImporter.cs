545c85a74b7f20826d01588600a9690b98d344ea
Karl Harnagy <kharnagy@inedo.com>
2015-11-12 15:33:41 -0500

    added operations

---
+++ ActionImporters/WriteAssemblyVersionsImporter.cs
@@ -0,0 +1,21 @@
+﻿using Inedo.BuildMaster.Extensibility.Operations;
+using Inedo.BuildMasterExtensions.WindowsSdk.DotNet;
+using Inedo.BuildMasterExtensions.WindowsSdk.Operations.DotNet;
+
+namespace Inedo.BuildMasterExtensions.WindowsSdk.ActionImporters
+{
+    internal sealed class WriteAssemblyVersionsImporter : IActionOperationConverter<WriteAssemblyInfoVersionsAction, WriteAssemblyInfoVersionsOperation>
+    {
+        public ConvertedOperation<WriteAssemblyInfoVersionsOperation> ConvertActionToOperation(WriteAssemblyInfoVersionsAction action, IActionConverterContext context)
+        {
+            var mask = context.ConvertLegacyMask(action.FileMasks, action.Recursive);
+            return new WriteAssemblyInfoVersionsOperation
+            {
+                Includes = mask.Includes,
+                Excludes = mask.Excludes,
+                SourceDirectory = action.OverriddenSourceDirectory,
+                Version = action.Version
+            };
+        }
+    }
+}
